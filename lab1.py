69568ba69ef01bb8fd46fc8c18cdd4812264e8ed
tushoo19 <39065814+tushoo19@users.noreply.github.com>
2019-12-26 09:16:14 -0800

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,11 @@
+#
+
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name: Tushar Sharma
+# Instructor: Julie Workman
+# Section:CPE 101-07
+#
+print("Hello, Tushar.")
